4efdb9dce63f233046236759c2e2420adfe900ff
Bonnie Nguyen <bonnie.nguyen@ynap.com>
2019-04-24 15:15:54 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="App">
+    <div class="App-Container">
+      <img class="Logo" :src="logo" alt="Vue logo"/>
+      <h1 class="App-Header">Vue To Do</h1>
+      <div>
+        <div>
+          <input class="input" placeholder="Task" v-model="todo"/>
+          <span class="btn btn-success" @click=addItem()> Add </span>
+        </div>
+        <div>
+          <ul class="ToDo-List">
+            <ToDoItem v-for="todo in todos"
+                      :todo="todo"
+                      @delete="OnDeleteItem(todo)"
+                      :key="todo.Id"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import ToDoItem from './components/ToDoItem.vue'
+import Logo from './assets/logo.png';
 
 export default {
-  name: 'app',
+  name: 'to-do',
   components: {
-    HelloWorld
+    ToDoItem
+  },
+  data() {
+      return {
+          logo: Logo,
+          todo: '',
+          todos: []
+      }
+  },
+  methods: {
+    addItem() {
+      if (this.todo === '') {
+        return
+      }
+
+      var newID = this.todos.length
+      this.todos = this.todos.concat({Id: newID, Item: this.todo})
+    },
+    OnDeleteItem(todo) {
+      this.todos = this.todos.filter(item => item !== todo)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica, Arial, sans-serif;
+    height: auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Logo {
+    width: 90px;
+    position: relative;
+    top: 50px;
+  }
+
+  .App {
+    text-align: center;
+    border: 1px solid white;
+    width: 80vw;
+    height: auto;
+    background: #ffffff;
+    padding-bottom: 60px;
+    margin: 40px auto;
+  }
+
+  .App-Header {
+    color: #00bb7e;
+    font-weight: 500;
+    margin: 70px auto 30px;
+    font-size: 2.5em;
+  }
+
+  .App-Container {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  input {
+    width: 45%;
+    padding: 10px;
+    font-size: 1em;
+    height: 3em;
+    margin: 10px auto;
+  }
+
+  .ToDo-List {
+    display: inline-flex;
+    justify-content: center;
+    flex-direction: column-reverse;
+    align-items: center;
+    list-style: none;
+  }
 </style>
+
+
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <li class="ToDoItem-Text" @click="$emit('delete', todo)">{{todo.Item}}</li>
+</template>
+
+<script>
+  export default {
+      name: "to-do-item",
+      props: ['todo']
+  }
+</script>
+
+<style>
+  .ToDoItem-Text {
+    color: #000000;
+    background-color: #adb7bf;
+    padding: 15px;
+    margin-bottom: 15px;
+    border-radius: 5px;
+    width: auto;
+  }
+
+  .ToDoItem-Text:hover {
+    background-color: pink;
+    cursor: pointer;
+  }
+</style>
+
